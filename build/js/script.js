86b916d2869ad24b6ad3b865a3ea7647c485e7d8
Anna <korobovaanna89@gmail.com>
2021-10-01 01:19:19 +0300

    Аккордион и блок

--- build/js/script.js
+++ build/js/script.js
@@ -13,23 +13,66 @@ headerBurger.addEventListener('click', function () {
     header.classList.remove('header__opened');
     header.classList.add('header__closed');
   }
-})
+});
 
 //Slider
 
-const swiper = new Swiper('.swiper', {
-  navigation: {
+(function () {
+  const swiper = new Swiper('.mySwiper', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      type: 'custom',
+      renderCustom: function (swiper, current, total) {
+        return current + '&nbsp; of &nbsp;' + (total);
+      }
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 2,
+        pagination: {
+          type: 'bullets',
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          }
+        }
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 4,
+        pagination: {
+          type: 'bullets',
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+          }
+        }
+      }
+    },
+    navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'fraction',
-    clickable: true,
-  },
-  slideToClickedSlide: true,
-  slidesPerView: 4,
-});
+  });
+
+  window.addEventListener('resize', function () {
+    swiper.pagination.update();
+  });
+})();
 
 
 
+const accordions = document.querySelectorAll('.accordion-item');
+
+  for (let item of accordions) {
+    item.addEventListener('click', function() {
+      this.classList.toggle('active')
+    })
+  }
